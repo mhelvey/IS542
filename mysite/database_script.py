ff92bd6df299d9bed1e7d7e67507c67dd1a90818
mhelvey <michael@helvey.net>
2015-01-12 11:00:25 -0700

    Added start of database script: 2 professors and 2 students

---
+++ mysite/database_script.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import os
+
+# initialize the django environment
+# assumes ./proj/settings.py is your settings file, relative to current dir
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+import django
+django.setup()
+
+# import models and run django/python commands
+from homepage import models as hmod
+try:
+    professor = hmod.Professor.objects.get(email='yoda@jedi.com')
+    print("Professor " + professor.name + " already exists")
+except hmod.Professor.DoesNotExist:
+    professor = hmod.Professor()
+    professor.name = "Yoda"
+    professor.address = "Degobah"
+    professor.email = "yoda@jedi.com"
+    professor.phone = "1234567890"
+    professor.salary = 100000
+    professor.save()
+    print("Professor " + professor.name + " created")
+
+try:
+    professor = hmod.Professor.objects.get(email='obi@jedi.com')
+    print("Professor " + professor.name + " already exists")
+except hmod.Professor.DoesNotExist:
+    professor = hmod.Professor()
+    professor.name = "Obi-Wan"
+    professor.address = "Tatooine"
+    professor.email = "obi@jedi.com"
+    professor.phone = "0123456789"
+    professor.salary = 75000
+    professor.save()
+    print("Professor " + professor.name + " created")
+
+try:
+    student = hmod.Student.objects.get(email='luke@jedi.com')
+    print("Student " + student.name + " already exists")
+except hmod.Student.DoesNotExist:
+    student = hmod.Student()
+    student.name = "Luke Skywalker"
+    student.address = "Tatooine"
+    student.email = "luke@jedi.com"
+    student.phone = "2345678901"
+    student.student_num = '12345'
+    student.average_mark = 3.85
+    student.save()
+    print("Student " + student.name + " created")
+
+try:
+    student = hmod.Student.objects.get(email='han@jedi.com')
+    print("Student " + student.name + " already exists")
+except hmod.Student.DoesNotExist:
+    student = hmod.Student()
+    student.name = "Han Solo"
+    student.address = "Corellia"
+    student.email = "han@jedi.com"
+    student.phone = "3456789012"
+    student.student_num = '23456'
+    student.average_mark = 3.35
+    student.save()
+    print("Student " + student.name + " created")
