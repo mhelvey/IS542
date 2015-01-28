24e2085b5628f3d3ae47e8209cacef1f2d7b2215
mhelvey <michael@helvey.net>
2015-01-28 09:01:42 -0700

    Added homework files

---
+++ homework/webspider.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import re, requests, json
+
+results = {}
+root_url = 'http://www.sec.gov'
+#Using requests Library, connect to http://www.sec.gov/divisions/enforce/friactions/friactions2002.shtml
+url1 = requests.get('http://www.sec.gov/divisions/enforce/friactions/friactions2002.shtml')
+text = url1.text
+
+#use a regular expression to gather the first 5 enforcement urls (sec.gov/ligigations?)
+#store the urls in a list
+urls = re.findall('/litigation/.*htm', text)[:5]
+
+# Conon's REGEX from his key
+# RELEASE_NO = re.compile('Release No [^\s]*\s*(\d+)', re.IGNORECASE | re.DOTALL | re.MULTILINE)
+# CASE_NAME = re.compile('Securities and Exchane Commission v\.\s*([A-Za-z0-9\s\.\,]+)', re.IGNORECASE | re.DOTALL)
+# AMOUNTS = re.compile('(\$[0-9\s]+\s*(billion|million|thousand)\*)', re.DOTALL | re.MULTILINE)
+#using a for loop, connect to each url in the list
+for url in urls:
+    url = root_url + url
+    response = requests.get(url)
+    text = response.text
+
+    #again using regex, pull the following from each enforcement page
+    # 1.	The release number (pull from the text, not from the url).
+    # 2.	The case name of the enforcement (the text that starts with Securities and Exchange Commission v…).
+    # 3.	All dollar amounts specified in the page.  A dollar amount starts with the dollar sign ($), contains numbers and commas, and may end with the text “thousand, million, billion”.
+    release_num = re.search("Release No.  (.*) /", text)
+    case_name = re.search("(Securities and Exchange Commission v. ([\w\s\.]*))", text)
+    if case_name is not None:
+        case_name = case_name.group(1)
+    if case_name is None:
+        case_name = 'not found'
+    amounts = re.findall("(\$[0-9\,\.]+\s*(billion|million|thousand)*)", text)
+
+    #create a dictonary for the items you gather from above
+    # if items do not exist, simply record "not found" for its value
+    # if multiple release numbers exist on the page, record only the first.
+    # key the dict by the URL
+    result = { url : {
+                    'release number': int(release_num.group(1)),
+                    'case name': case_name,
+                    'amounts': amounts,
+                    }
+    }
+    results.update(result)
+
+#print the dict to the console using json-dumps()
+print(json.dumps(results))
+
+#print it again using for loops to print it manually
+#found this loop at http://stackoverflow.com/questions/15785719/how-to-print-a-dictionary-line-by-line-in-python
+for keys in results:
+    print(keys)
+    for values in results[keys]:
+        print(values,':',results[keys][values])
+    print("\n")
